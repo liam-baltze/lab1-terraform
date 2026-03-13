c3d1c680e6aa0c6b31770d001dcae882afaa01a6
liam-baltze <liam.baltze@chasacademy.se>
2026-03-13 22:00:22 +0100

    trigger pipeline

--- variables.tf
+++ variables.tf
@@ -9,6 +9,12 @@ variable "region" {
   default     = "europe-north1"
 }
 
+variable "zone" {
+  description = "GCP Zone"
+  type        = string
+  default     = "europe-north1-a"
+}
+
 variable "student_id" {
   description = "Your student identifier"
   type        = string
